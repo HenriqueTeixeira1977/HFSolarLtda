4d7234725ecdfe0c81d3d7115c28a72869597eb9
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-05-24 03:05:29 -0300

    Update202405240305

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,11 @@ document.getElementById('solar-form').addEventListener('submit', function(e) {
 
 });
 
+ Google tag (gtag.js)
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2HJZTFTFZ7');
+
