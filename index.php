f179903ca4552ff9c622a846bb6fc58ebe7f1c51
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2025-02-08 11:42:39 -0300

    Update20250208114211

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HF SOLAR LTDA - Economize em até 90% em sua conta de luz!!!</title>
+</head>
+<body>
+    <h1>HF SOLAR - HOME</h1>
+    
+</body>
+</html>
